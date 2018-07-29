49aa4a57f21914cf4b7a8a28e5ac6e4ff36fb99f
paradox3d <41789216+paradox3d@users.noreply.github.com>
2018-07-29 23:57:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
- let receiveBtn = document.getElementById("receive"),
-     modal = document.querySelector(".modal"),
-     close = document.querySelector(".close"),
-     nameInput = document.getElementsByClassName("contactform_name")[0],
-     text = document.getElementsByName("message")[0];
+window.addEventListener("DOMContentLoaded", function() {
+
+	let products = document.querySelectorAll(".product"),
+	    buttons = document.getElementsByTagName("button"),
+	    open = document.getElementsByClassName("open")[0];
+
+	    function createCart() {
+	    	let cart = document.createElement("div"),
+	    		field = document.createElement("div"),
+	    		heading = document.createElement("h2"),
+	    		close = document.createElement("button");
+
+	    	cart.classList.add("cart");
+	    	field.classList.add("cart-field");
+	    	close.classList.add("close");
+
+	    	close.textContent = "Закрыть";
+	    	heading.textContent = "В нашей корзине";
+
+	    	document.body.appendChild(cart);
+	    	 cart.appendChild(heading);
+	    	 cart.appendChild(field);
+	    	 cart.appendChild(close);
 
 
+	    }
 
-receiveBtn.addEventListener("click", function() {
-	modal.style.display = "block";
-});
+	    createCart();
 
-close.addEventListener("click",function() {
-	modal.style.display = "none";
-})
 
-nameInput.addEventListener("input", function() {
-	text.value = "Мое имя " + nameInput.value + ". И я хочу спросить: ";
+         let field = document.querySelector(".cart-field");
+         	 cart = document.querySelector(".cart");
+         	 close	 = document.querySelector(".close");
 
-	if (nameInput.value == ""){
-		text.value = ""
-	}
+
+         	 for (let i = 0; i < buttons.length; i++) {
+
+         	 	buttons[i].addEventListener("click",function() {
+         	 		let item = products[i].cloneNode(true),
+         	 			btn = item.querySelector("button");
+
+         	 			btn.remove();
+         	 			field.appendChild(item);
+         	 			products[i].remove();
+         	 			})
+
+         	 		}
+
+
+         	 	function openCart() {
+         	 		cart.style.display = "block";
+					};
+
+
+         	 	function closeCart() {
+         	 		cart.style.display = "none";
+					};
+
+         	 	open.addEventListener("click", openCart);
+         	 	close.addEventListener("click", closeCart);
 });
 
